de8d6a66db2157f48b5987570b6c1899075c745f
Danilo <dlineros@forum.cl>
2024-11-28 15:11:50 -0300

    plantilla completa con rutas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    RouterOutlet,
+
+
+  ],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
