0ca262fb930e27641f753d63c321ad400e68dc0b
Danilo <dlineros@forum.cl>
2024-11-28 10:58:58 -0300

    template funcionando sin rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, NavbarComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
