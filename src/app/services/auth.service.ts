653f6e6e38ca70fc30e5e29f293740275c231c55
Danilo <dlineros@forum.cl>
2024-11-28 17:39:17 -0300

    conectado con firebase en registro

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { inject } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root', // Este servicio estará disponible de forma global
 })
 export class AuthService {
+  private auth = inject(Auth);
 
-  constructor(public auth: AngularFireAuth) {  }
+  constructor() {}
 
-  crearUsuario(nombre:string, paterno:string,materno:string,correo:string, password:string)
-      {
-        //console.log(nombre,paterno,materno,password);
-        this.auth.signInWithEmailAndPassword(correo,password)
-        .then(credenciales=>{
-          console.log(credenciales);
-        })
-        .catch(err=>console.error(err));
-      }
+  // Método para iniciar sesión
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
 
+  // Método para registrarse
+  crearUsuario(nombre: string, paterno:string, materno:string,correo: string, password: string) {
+    console.log(this.auth);
+    return createUserWithEmailAndPassword(this.auth, correo, password);
+  }
+
+  // Método para cerrar sesión
+  logout() {
+    return signOut(this.auth);
+  }
 }
